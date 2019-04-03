0c9f04bcc788dfc04e4d980cb10fcf1ddaa5b669
workingjubilee <workingjubilee@gmail.com>
2019-04-03 13:51:46 -0700

    Adding migrations

---
+++ migrations/20190327104720_roles.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('roles', function(tbl) {
+    tbl.increments();
+
+    tbl
+      .string('name',128)
+      .notNullable()
+      .unique();
+
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
